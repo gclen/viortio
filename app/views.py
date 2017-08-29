7f24952cefcfe3380f3e1b8392f1b570aa2ef730
gclen <graham.clendenning@uoit.net>
2017-08-28 20:35:07 -0400

    Added some error handling

--- app/views.py
+++ app/views.py
@@ -119,6 +119,14 @@ def project(project_name):
     return render_template('project.html', tasks=project_tasks, title=project_name)
 
 
+@app.errorhandler(404)
+def file_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 
