b12504564cf4504e4e60e7b57a699dd053844b1b
gclen <graham.clendenning@uoit.net>
2017-09-02 16:02:27 -0400

    Added REST API

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
+import json
 from datetime import datetime
 from sqlalchemy import desc
 from app import db
+
+
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.strftime('%Y-%m-%d %H:%M:%S')
+        return json.JSONEncoder.default(self, o)
 
 
 class User(db.Model):
@@ -33,9 +41,9 @@ class User(db.Model):
     def get_projects(self):
         return Task.query.with_entities(Task.project.distinct()).filter_by(user_id=self.id).filter(Task.project != None).order_by('project').all()
 
+
     def __repr__(self):
         return '<User {}>'.format(self.nickname)
-
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,6 +53,11 @@ class Task(db.Model):
     project = db.Column(db.String(140), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     complete = db.Column(db.Boolean, default=False)
+
+    def to_JSON(self):
+        d = {'id': self.id, 'name': self.name, 'due_date': self.due_date, 'start_date': self.start_date,
+             'project': self.project, 'complete': self.complete}
+        return json.dumps(d, sort_keys=True, cls=DatetimeEncoder)
 
     def __repr__(self):
         return 'Task is: {}'.format(self.name)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, session, request, g
+from dateutil.parser import parse
+from flask import render_template, flash, redirect, url_for, session, request, g, jsonify, abort
 from flask_login import login_required, logout_user, login_user, current_user
+from flask_httpauth import HTTPBasicAuth
 from flask_babel import gettext
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import app, db, lm
@@ -8,6 +10,7 @@ from .task import Task
 from .forms import TaskForm, LoginForm, RegistrationForm
 from .models import User, Task
 
+auth = HTTPBasicAuth()
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -130,8 +133,119 @@ def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
 
+# Creating a REST API
+@app.route('/viortio/api/v1.0/users/<int:id>', methods=['GET'])
+def get_user(id):
+    u = User.query.get(id)
+    if not u:
+        abort(400)
+    return jsonify({'username': u.nickname})
 
 
+@app.route('/viortio/api/v1.0/register', methods=['POST'])
+def register_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if username is None or password is None:
+        abort(400)
+    if User.query.filter_by(nickname=username).first() is not None:
+        abort(400)
+
+    user = User(nickname=username, password_hash=generate_password_hash(password))
+    db.session.add(user)
+    db.session.commit()
+
+    return (jsonify({'username': user.nickname}), 201, {'Location': url_for('get_user', id=user.id, _external=True)})
+
+@app.route('/viortio/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    tasks = [t.to_JSON() for t in g.user.get_tasklist()]
+    return jsonify({'tasks': tasks})
+
+@auth.verify_password
+def verify_password(username, password):
+    u = User.query.filter_by(nickname=username).first()
+    if not u or not check_password_hash(u.password_hash, password):
+        return False
+    g.user = u
+    return True
 
 
+@app.route('/viortio/api/v1.0/tasks/create', methods=['POST'])
+@auth.login_required
+def create_task():
+    if not request.json or not 'name' in request.json:
+        abort(400)
 
+    due_date = parse(request.json.get('due_date')) if request.json.get('due_date') else None
+    start_date = parse(request.json.get('start_date')) if request.json.get('start_date') else datetime.utcnow()
+
+    t = Task(name=request.json.get('name'), due_date=due_date, start_date=start_date,
+             project=request.json.get('project', None), user_id=g.user.id)
+
+    db.session.add(t)
+    db.session.commit()
+
+    return jsonify(t.to_JSON()), 201
+
+@app.route('/viortio/api/v1.0/tasks/update/<int:id>', methods=['POST'])
+@auth.login_required
+def update_task(id):
+
+    t = Task.query.filter_by(id=id, user_id=g.user.id).first()
+
+    if t is None:
+        abort(404)
+
+    if request.json.get('name'):
+        t.name = request.json.get('name')
+    if request.json.get('due_date'):
+        t.due_date = parse(request.json.get('due_date'))
+    if request.json.get('start_date'):
+        t.start_date = parse(request.json.get('start_date'))
+    if request.json.get('project'):
+        t.project = request.json.get('project')
+    if request.json.get('complete'):
+        t.complete = request.json.get('complete')
+
+    db.session.commit()
+
+    return jsonify(t.to_JSON()), 201
+
+
+@app.route('/viortio/api/v1.0/tasks/delete/<int:id>', methods=['POST'])
+@auth.login_required
+def delete_task(id):
+
+    t = Task.query.filter_by(id=id, user_id=g.user.id).first()
+    if t is None:
+        abort(404)
+
+    db.session.delete(t)
+    db.session.commit()
+
+    return jsonify({'deleted': id}), 201
+
+
+@app.route('/viortio/api/v1.0/tasks/completed', methods=['GET'])
+@auth.login_required
+def get_completed_tasks():
+    tasks = [t.to_JSON() for t in g.user.get_completed_tasks()]
+    return jsonify({'completed': tasks})
+
+
+@app.route('/viortio/api/v1.0/projects', methods=['GET'])
+@auth.login_required
+def get_projects():
+    projects = [p[0] for p in g.user.get_projects()]
+    return jsonify({'projects': projects})
+
+
+@app.route('/viortio/api/v1.0/projects/<project_name>', methods=['GET'])
+@auth.login_required
+def get_tasks_for_project(project_name):
+    project_tasks = Task.query.filter_by(project=project_name, complete=False).order_by('start_date').all()
+    project_tasks = [t.to_JSON() for t in project_tasks]
+    return jsonify({'project': project_name, 'tasks': project_tasks})
